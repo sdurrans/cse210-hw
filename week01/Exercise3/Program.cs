f7b88f27313c049c86663afcf0512ca02de2872c
sdurrans <stuartdurrant@gmail.com>
2025-01-10 00:13:07 -0500

    steps 1 and 2

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Diagnostics.CodeAnalysis;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.Write("What is the magic number? ");
+        string startNumber = Console.ReadLine();
+        int magicNumber =int.Parse(startNumber);
+        
+        int userGuess = -1;
+        while (magicNumber != userGuess)
+        {
+            Console.Write("What is your guess? ");
+            string guess =Console.ReadLine();
+            userGuess =int.Parse(guess);
+        
+            if (magicNumber > userGuess)
+            {
+                Console.WriteLine ("Higher");
+            }
+            else if (magicNumber < userGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+                Console.WriteLine("You guessed it!");
+        }
     }
 }
