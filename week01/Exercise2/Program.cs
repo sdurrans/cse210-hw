01fcdb6a50d0e2ff98092b84b8db44d780c73296
sdurrans <stuartdurrant@gmail.com>
2025-01-09 21:50:01 -0500

    step 2 exercise 2 wk1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string userGrade = Console.ReadLine();
+        int grade =int.Parse(userGrade);
+
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade is and A");
+        }
+
+        else if (grade >= 80 && grade < 90)
+        {
+            Console.WriteLine("Your grade is a B");
+        }
+        else if (grade >= 70 && grade < 80)
+        {
+            Console.WriteLine("Your grade is a C");
+        }
+        else if (grade >= 60 && grade < 70)
+        {
+            Console.WriteLine("Your grade is a D");
+        }
+        else 
+        {
+            Console.WriteLine("Your grade is a F");
+        }
+
+        if (grade >=70)
+        {
+            Console.WriteLine("Congradulation you passed the class! ");
+        }
+        else 
+        {
+            Console.WriteLine("Your grade is too low, you failed. Try again.");
+        }
     }
 }
