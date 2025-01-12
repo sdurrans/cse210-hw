cdd59c702744e72303f21e4594473e30dee9d130
sdurrans <stuartdurrant@gmail.com>
2025-01-11 21:44:37 -0500

    final lists

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,52 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a series of numbers. Enter 0 to stop:");
+
+        int userNumbers = -1;
+
+        while (userNumbers !=0)
+        {
+            Console.Write("Enter a number: ");
+            string input = Console.ReadLine();
+
+            userNumbers = int.Parse(input);
+
+            if (userNumbers !=0)
+            {
+                numbers.Add(userNumbers);
+            }
+        }
+        int total = 0;
+        foreach (int number in numbers)
+        {
+            total += number;
+        }
+
+        Console.WriteLine($"The sum is: {total}");
+
+        int average= -1;
+        int numbersCount = numbers.Count;
+
+        average= total/numbersCount;
+
+        Console.WriteLine($"The average is: {average}");        
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
