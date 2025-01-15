a4bd1cfadea608ca64e557ce3b1e59c0ba6fff5b
sdurrans <stuartdurrant@gmail.com>
2025-01-14 22:01:05 -0500

    jobs 1 and 2 created

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Job job1 = new Job();
+        job1._jobTitle= "Mechanical Engineer";
+        job1._company= "Moog";
+        job1._startYear= 2020;
+        job1._endYear= 2035;
+
+        Job job2 = new Job();
+        job2._jobTitle= "Software Deveolper";
+        job2._company= "Halo Studios";
+        job2._startYear= 2035;
+        job2._endYear= 2050;
     }
-}
+} 
