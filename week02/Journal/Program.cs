fa137b81230bbbb3f5b07112bce6a73d0169f8f9
sdurrans <stuartdurrant@gmail.com>
2025-01-16 21:25:05 -0500

    progam main loop

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -7,6 +7,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello this is the Journal Program");
+        Console.WriteLine("Welcome to the Journal Program! .");
+        Console.WriteLine("Please select of the the Following choices. ");
+
+        while (true)
+        {
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+
+            Console.WriteLine("What would you like to do? ");
+            string choice= Console.ReadLine();
+
+            if (choice == "1")
+            {
+                
+                
+            
+            }
+            else if (choice == "2")
+            {
+                
+            }
+            else if (choice == "3")
+            {
+               
+            }
+            else if (choice == "4")
+            {
+                
+            }
+            else if (choice == "5")
+            {
+                break;
+            }
+        }
     }
 }
