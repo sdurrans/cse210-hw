262ce407f55091642a669d04e53f5e8b8282398a
sdurrans <stuartdurrant@gmail.com>
2025-01-16 21:18:15 -0500

    starting over after team meeting

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,59 +1,12 @@
 using System;
 using System.IO;
+using System.Net.Security;
 using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        //Journal journal = new Journal();
-        PromptGenerator promptGenerator = new PromptGenerator();
-
-        Console.WriteLine("Welcome to the Journal Program! .");
-        Console.WriteLine("Please select of the the Following choices. ");
-
-        while (true)
-        {
-            Console.WriteLine("1. Write");
-            Console.WriteLine("2. Display");
-            Console.WriteLine("3. Load");
-            Console.WriteLine("4. Save");
-            Console.WriteLine("5. Quit");
-
-            Console.WriteLine("What would you like to do? ");
-            string choice= Console.ReadLine();
-
-            if (choice == "1")
-            {
-                
-                string prompt = promptGenerator.GetRandomPrompt();
-                Console.WriteLine($"Prompt: {prompt}");
-                Console.Write("Your response: ");
-                string response = Console.ReadLine();
-                string date = DateTime.Now.ToString("yyyy-MM-dd");
-                //journal.AddEntry(new Entry(date, prompt, response));
-            
-            }
-            else if (choice == "2")
-            {
-                //DisplayJournal();
-                Console.WriteLine("Run display code");
-                journal.DisplayAll();
-            }
-            else if (choice == "3")
-            {
-                //SaveJournal();
-                Console.WriteLine("Run load code");
-            }
-            else if (choice == "4")
-            {
-                //LoadJournal();
-                Console.WriteLine("Run save code");
-            }
-            else if (choice == "5")
-            {
-                break;
-            }
-        }
+        Console.WriteLine("Hello this is the Journal Program");
     }
 }
--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,25 +1,9 @@
-using System;
-using System.Collections.Generic;
-
 public class PromptGenerator
 {
-    private List<string> _prompts = new List<string>
-    {
-        "Who was the most interesting person I interacted with today?",
-        "What was the best part of my day?",
-        "How did I see the hand of the Lord in my life today?",
-        "What was the strongest emotion I felt today?",
-        "If I had one thing I could do over today, what would it be?",
-        "What is one habit I want to build, and how will it improve my life?",
-        "Write a letter to your future self about what you are experiencing today.",
-        "Imagine a conversation between your present self and your 10-year-old self. What would you say?",
-        "What is one way I can show love or appreciation to someone I care about?",
-    };
+    public List<string> _prompts;
 
     public string GetRandomPrompt()
     {
-        Random random = new Random();
-        int index = random.Next(_prompts.Count);
-        return _prompts[index];
+        return "";
     }
 }
