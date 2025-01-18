81d1c4337a7c1459156ad4333f468023bd3d4d9e
sdurrans <stuartdurrant@gmail.com>
2025-01-17 19:55:40 -0500

    exceed requirements code.

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,6 +1,10 @@
 using System;
 using System.IO;
 
+// to exceed requirements i thought it would be nice to cycle through all prompts before one repeats. so i added code to do that in the PromptGernerator and Programs code. I comment out the core requiremetn code.  
+
+
+
 class Program
 {
     static void Main(string[] args)
@@ -24,14 +28,30 @@ class Program
 
             if (choice == "1")
             {
+                //string prompt = promptGenerator.GetRandomPrompt();
+                //string response = Console.ReadLine();
+                //journal.AddEntry(new Entry
+                //{
+                    //_date = DateTime.Now.ToString("yyyy-MM-dd"),
+                    //_promptText = prompt,
+                    //_entryText = response
+                //});
+
+                //stretch requirement cycleing through all propmts until they are all used.
                 string prompt = promptGenerator.GetRandomPrompt();
+                Console.WriteLine($"Prompt: {prompt}");
+                Console.Write("Your response: ");
                 string response = Console.ReadLine();
-                journal.AddEntry(new Entry
+
+                Entry newEntry = new Entry
                 {
                     _date = DateTime.Now.ToString("yyyy-MM-dd"),
                     _promptText = prompt,
                     _entryText = response
-                });
+                };
+
+            journal.AddEntry(newEntry);
+
             }
             else if (choice == "2")
             {
--- week02/Journal/PromptGenerator.cs
+++ week02/Journal/PromptGenerator.cs
@@ -1,6 +1,7 @@
 public class PromptGenerator
 {
-    public List<string> _prompts = new List<string>(){
+    public List<string> _prompts = new List<string>()
+    {
         "Who was the most interesting person I interacted with today?",
         "What was the best part of my day?",
         "How did I see the hand of the Lord in my life today?",
@@ -12,15 +13,37 @@ public class PromptGenerator
         "What is one way I can show love or appreciation to someone I care about?",
     };
 
+    List<string> _unusedPrompts;
+    Random _random = new Random();
+
+    public PromptGenerator()
+    {
+        _unusedPrompts = new List<string>(_prompts);
+    }
 
     public string GetRandomPrompt()
     {
         // Get a random index value and lookup the string in _prompts
-        Random rand = new Random();
-        int randomIndex = rand.Next(_prompts.Count); 
-        string prompt = _prompts[randomIndex]; 
-        Console.WriteLine($"Prompt: {prompt}");
-        Console.Write("> ");
-        return prompt;
+        //Random rand = new Random();
+        //int randomIndex = rand.Next(_prompts.Count); 
+        //string prompt = _prompts[randomIndex]; 
+       //Console.WriteLine($"Prompt: {prompt}");
+       //Console.Write("> ");
+       //return prompt;
+
+       // stretch requirtement, cycle through all prompts beofre one gets repeated. 
+       if (_unusedPrompts.Count == 0)
+        {
+            _unusedPrompts = new List<string>(_prompts);
+        }
+
+        int randomIndex = _random.Next(_unusedPrompts.Count);
+        string selectedPrompt = _unusedPrompts[randomIndex];
+
+         _unusedPrompts.RemoveAt(randomIndex);
+
+         return selectedPrompt;
+
     }
+
 }
