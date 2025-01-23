475bfc3c6d6d2ff6329129d19b2029985ac770ad
sdurrans <stuartdurrant@gmail.com>
2025-01-23 18:22:36 -0500

    fraction class initiated

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,33 @@
+using System;
+
+public class Fraction
+{
+        private int _top;
+        private int _bottom;        
+       
+
+    public Fraction()
+    {
+        
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        
+    }
+
+    public string GetFractionString()
+    {
+        
+    }
+
+    public double GetDecimalValue()
+    {
+        
+    }    
+}
