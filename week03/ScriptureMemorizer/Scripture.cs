6a0b235746e64769123965b60d755b9b028ca23d
sdurrans <stuartdurrant@gmail.com>
2025-01-23 22:14:24 -0500

    scripture diagram

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,28 @@
+using System;
+
+class Scripture
+{
+    private Reference _reference;
+    private Word[] _words;
+
+    public Scripture(Reference reference, string text)
+    {
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+
+    }
+
+    public string GetDisplayText()
+    {
+        return "Scripture display text";
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        return true;
+    }
+
+}
