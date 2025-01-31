3b4727d1aa59af41ae44ff4c8184846cacfca0b1
sdurrans <stuartdurrant@gmail.com>
2025-01-30 20:55:34 -0500

    comments skeleton

--- week04/YouTubeVideos/Comments.cs
+++ week04/YouTubeVideos/Comments.cs
@@ -2,5 +2,11 @@ using System;
 
 public class Comments
 {
-    
+    public string Person { get;private set;}
+    public string Text {get; private set;}
+
+    public Comments(string person, string text)
+    {
+        
+    }
 }
