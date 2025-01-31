8d66f67a39f95a509bdde2d677cd980e51443612
sdurrans <stuartdurrant@gmail.com>
2025-01-30 21:35:24 -0500

    video list skeleton

--- week04/YouTubeVideos/VideoList.cs
+++ week04/YouTubeVideos/VideoList.cs
@@ -2,5 +2,10 @@ using System;
 
 public class VideoList
 {
-    
+    public string Title {get; private set;}
+    public string Author {get; private set;}
+    public int length {get; private set;}
+
+    public List<Comment> Comments {get; private set;}
+
 }
