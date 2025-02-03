43e1927a32b5102652c94e098522f8813087ad95
sdurrans <stuartdurrant@gmail.com>
2025-02-02 20:27:36 -0500

    order final

---
+++ week04/OnlineOrdering/Order.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+
+class Order
+{
+    private List<Product> products;
+    private Customer customer;
+    private const decimal USA_SHIPPING_COST = 5m;
+    private const decimal INTERNATIONAL_SHIPPING_COST = 35m;
+
+    public Order(Customer customer)
+    {
+      
+    }
+
+    public void AddProduct(Product product)
+    {
+       
+    }
+
+    public decimal GetTotalCost()
+    {
+        
+    }
+
+    public string GetPackingLabel()
+    {
+        
+    }
+
+    public string GetShippingLabel()
+    {
+        
+    }
+}
