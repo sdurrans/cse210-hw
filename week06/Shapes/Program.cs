67fe3188251d464add9fa4195823b5ab0ceae860
sdurrans <stuartdurrant@gmail.com>
2025-02-11 21:34:06 -0500

    shapes finished

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
+
+        Rectangle r1 = new Rectangle("Blue", 5,3);
+        shapes.Add(r1);
+
+        Circle c1 = new Circle("Green", 2);
+        shapes.Add(c1);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"Color: {color}");
+            Console.WriteLine($"Area: {area}");
+            
+        }
     }
 }
